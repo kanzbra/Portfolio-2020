705ea912b7c8687eba92db3a31af9737749c4080
kanzbra rashid <kanzbra.rashid@gmail.com>
2020-07-19 22:19:02 -0400

    bum

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function() {
+        $('.menu-toggler').toggleClass('open');
+        $('.top-nav').toggleClass('open').show();
+    });
+    $('.top-nav li').on('click', function(){
+        $(".top-nav").hide();
+    });
+
+});
+
+$(document).ready(function () {
+    $("video").css("height, 390px;"); 
+    $("#caseOne").hover(function(){
+        $("video").attr("src","./images/projects/fsh/FSH.mp4").load().play();
+    });
+        $("#caseTwo").hover(function(){
+        $("video").attr("src","./images/projects/giani/Giani.mp4").load().play();
+    });
+        $("#caseThree").hover(function(){
+        $("video").attr("src","./images/projects/holtfit/HoltFIt.mp4").load().play();
+    });
+    });
+    0
+
+
+    $(window).on('beforeunload', function() { $("video").hide(); }); 
