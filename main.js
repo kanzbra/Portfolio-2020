58cd570f09be979328ad772eb3fcc98b60b10da2
kanzbra rashid <kanzbra.rashid@gmail.com>
2020-08-11 21:59:59 -0400

    update

--- main.js
+++ main.js
@@ -22,6 +22,24 @@ $(document).ready(function () {
     });
     });
     
+var textWrapper = document.querySelector('.name');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+anime.timeline({loop: true})
+  .add({
+    targets: '.name .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.name',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
 
     $(window).on('beforeunload', function() { $("video").hide(); }); 
